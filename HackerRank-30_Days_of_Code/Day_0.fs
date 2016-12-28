0d98e2d1168722ccdc8b3f35a78f241453679c27
Abhinav Sharma <abhi18av@gmail.com>
2016-12-28 19:32:21 +0530

    the first challenge has been done

---
+++ HackerRank-30_Days_of_Code/Day_0.fs
@@ -0,0 +1,9 @@
+open System
+
+let main() =
+ //   Console.Write("What's your name? ")
+    let line = Console.ReadLine()
+    Console.Write("Hello, World.\n")
+    Console.Write(line)
+    
+main()
