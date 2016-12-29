409e78135a2f98a02bf0003a2563f004c7ca5ba4
Abhinav Sharma <abhi18av@gmail.com>
2016-12-29 22:29:26 +0530

    solved Day1 in python

---
+++ HackerRank-30_Days_of_Code/Day_1.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+
+# Declare second integer, double, and String variables.
+iTwo  = input()
+# Read and save an integer, double, and String to your variables.
+dTwo = input()
+sConcat = input()
+# Print the sum of both integer variables on a new line.
+print(i+int(iTwo))
+# Print the sum of the double variables on a new line.
+print(d + float(dTwo))
+print("HackerRank " + sConcat)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
